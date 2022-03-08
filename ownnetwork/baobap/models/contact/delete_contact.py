65989dd3455769502e332366bafdb6545a893733
iammehdib <100452676+iammehdib@users.noreply.github.com>
2022-03-08 23:49:14 +0100

    Init version

---
+++ ownnetwork/baobap/models/contact/delete_contact.py
@@ -0,0 +1,11 @@
+# MIT License -> LISCENCE.mit
+
+import json
+
+"""
+Delete a contact from json acccount file
+"""
+class DeleteContact(object):
+
+    def __init__(self, contact_id: str):
+        with open('w')
